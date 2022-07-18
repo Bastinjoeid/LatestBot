349bde48432cd18fa4d060a4d26364af30b361e5
Bastinjoeid <102047556+Bastinjoeid@users.noreply.github.com>
2022-07-18 14:20:16 +0530

    Update carbon.py

--- plugins/carbon.py
+++ plugins/carbon.py
@@ -5,10 +5,10 @@ from plugins.function import make_carbon
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 aiohttpsession = ClientSession()
 
-C = "**Made by @mkn_bots_updates**"
+C = "**Made by @sovel_jaison**"
 F = InlineKeyboardMarkup(
 [[
-     InlineKeyboardButton("𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝚄𝚂", url="https://t.me/mkn_bots_updates")
+     InlineKeyboardButton("Sᴜᴘᴘᴏʀᴛ Us ", url="https://t.me/cinemahub00")
 ]]
 )
 
